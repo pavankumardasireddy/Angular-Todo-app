b53a7197520e93a288dcb84a6e6a423da6206f97
pavankumardasireddy <pavankumar.dasireddy@gmail.com>
2020-02-11 17:42:23 +0530

    Implementation of Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { CoursesComponent } from './courses.component';
+import { TodosComponent } from './components/todos/todos.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CourseComponent } from './course/course.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TodosComponent},
+  { path: 'courses', component: CoursesComponent},
+  { path: 'org', component: CourseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
